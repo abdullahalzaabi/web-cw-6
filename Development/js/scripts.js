e2e67b25111c1539107b221f2a29296171d6cb8a
abdullahalzaabi <66646611+abdullahalzaabi@users.noreply.github.com>
2020-07-03 01:25:34 +0300

    ..

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -34,3 +34,96 @@ while (stillbuying) {
         console.log("thebill");
         console.log("thebill");
         console.log("quantity");
+
+    
+        let endshop ;
+let bag = [] ;
+while(endshop )
+{
+    let end =prompt("ادخل انتهيت عند الانتهاء من التسوق");
+    if(end="انتهيت")
+    {
+        let item=prompt("السلعة");
+        let price=prompt("السعر");
+
+        let pel={
+            end : end ,
+            price : price ,
+            item : item ,
+        };
+        bag.push(pel)
+    }
+    else (endshop = "انتهيت")
+    console.log(pel);
+}
+let stillBuying = true;
+let cart = [];
+while (stillBuying) {
+  let name = prompt("السلعه: اضغط انتهيت عندما تنتهي");
+  if (name != "انتهيت") {
+    let quantity = prompt("الكميه");
+    let price = prompt("السعر");
+
+    let item = {
+      name: name,
+      quantity: quantity,
+      price: price,
+    };
+    cart.push(item);
+  } else {
+    stillBuying = false;
+    printBill(cart);
+  }
+}
+function printBill(items) {
+  let totalPrice = 0;
+  console.log("الفاتوره");
+  console.log("------------");
+  for (let i = 0; i < cart.length; i++) {
+    console.log(
+      items[i]["quantity"] +
+        " " +
+        items[i]["name"] +
+        " " +
+        items[i]["price"] +
+        " KD"
+    );
+    totalPrice += parseFloat(items[i]["price"]);
+  }
+  console.log("السعر الكلي:" + totalPrice);
+  console.log("------------");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
